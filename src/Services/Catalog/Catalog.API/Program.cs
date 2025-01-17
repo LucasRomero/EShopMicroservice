6d5063c3c8de943aec0717b8977c08b09153a699
LucasRomero <romerolucasdaniel@outlook.com>
2025-01-17 16:59:35 -0300

    Fixing problem with Carter and its problem that it doesnt read the assembly, so we must have the package to be installed in each microservices.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container. DI
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.MapCarter();
 
 app.Run();
