63268c7be97f94d3efb829eeef09626a9f0f68c4
LucasRomero <romerolucasdaniel@outlook.com>
2025-01-17 16:31:38 -0300

    New changes. Integrated: MediatR Carter
    and globalusing class
    
    Also new project where we can share with all the microservices that is BuildingBlocks and use the common libraries

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS;
+
+
+// this ICommandHandler doent have a response, just return Unit from MediatR
+public interface  ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+// this ICommandHandler have a response 
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+{
+}
