1a8cdfebebb80bf9a70c2796f0ea13b71e33750a
LucasRomero <romerolucasdaniel@outlook.com>
2025-01-20 15:48:34 -0300

    Added Marten Library

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
-
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -27,13 +23,12 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
             Price = command.Price
         };
 
-        // TODO
         // 2. Save to database
-
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         // 3. Return CreateProductResult result
-
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
 
     }
 }
